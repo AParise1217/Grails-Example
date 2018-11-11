ce6d1d01fd06b4d5a84f6c17915f71cac8dbf480
Andrew Parise <parise.andrew@pennmutual.com>
2018-11-11 18:32:51 -0500

    Andrew Parise - Initial Commit

---
+++ grails-app/controllers/grails/example/PersonController.groovy
@@ -0,0 +1,14 @@
+package grails.example
+
+/**
+ * <h1>Person Controller</h1>
+ * <p>REST Controller for Performing CRUD Operations against {@link Person} Domain Models</p>
+ *
+ * @author  Andrew Parise <andrewparise1994@gmail.com>
+ * @since   11 November 2018
+ */
+class PersonController {
+
+    static scaffold = Person
+
+}
